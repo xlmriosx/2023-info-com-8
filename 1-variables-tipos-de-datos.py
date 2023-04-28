69bb71342fbdf51c2cd2cf45bd78991517a83d4e
unknown <xlmriosx@gmail.com>
2023-04-28 17:58:38 -0300

    semana 1 - 2, speed run

---
+++ 1-variables-tipos-de-datos.py
@@ -0,0 +1,48 @@
+print("hola mundo")
+
+# Variables y tipos datos
+
+### Tipos de datos simples
+enteros = 11 # int -> integer -> entero/// = es operador de asignacion
+reales = 11.22 # float -> reales (?)
+    # tipos string, cadenas de caracteres
+strings = 'hola'
+strings1 = "hola"
+strings3 = f"hola"
+strings4 = '''hola''' # strings4[0] devuelve h
+
+bool_verdadero = True
+bool_falso = False
+
+### Tipos de datos complejos
+listas = [1, 3, 4, 5] # 
+listas[0] # devuelve 1
+listas[1] # devuelve 3
+listas[2] # devuelve 4
+listas[3] # devuelve 5
+
+listas.sort() # ordeno la lista, tienen valores ordenables como por ejmplo, numeros
+listas.reverse() # listas = [5, 4, 3, 1]
+
+# agregar elementos
+listas.append(32)
+
+# sacar elementos
+listas = [32, 13, 10, 54]
+listas.pop() # elimino el ultimo elemento, elimino 54
+listas.pop(1) # saca por indice, en este caso el num 13
+# listas.remove(13) # saca por valor
+
+# son estructuras de datos complejas estaticas
+tuplas = (1, 3, 32, 13, 15)
+print('elemento 32 de la tupla:', tuplas[2])
+# tuplas[2]=23 # no se puede cambiar los valores de las tuplas
+# print(tuplas[2])
+
+valores_tuplas = list(tuplas)
+print(type(valores_tuplas)) # type() para saber el tipo de dato
+print("antes de cambiar", valores_tuplas)
+valores_tuplas[2]=23
+print("despues de cambiar", valores_tuplas)
+
+# Estudiarrrr --> dict, set
---
+++ 2-estructuras-de-control.py
@@ -0,0 +1,29 @@
+# estructuras condicionales
+# ... codigo, condicional simple
+# if(a==b): # si pasa esto, hace lo que esta debajo
+#     print("b y a son iguales")
+# else: # si no pasa lo de arriba hace esto
+#     print("b y a NO son iguales")
+
+# # condicional multiple
+# if(a==b):
+#     print("b y a son iguales")
+# elif(a*3==30):
+#     print("a es igual a 10")
+# elif(a/20 == 10):
+#     print("")
+# else:
+#     print("")
+
+# estructuras repetitivas
+# repetitiva con contador, rep. con condicional a priori, rep. con condicional posteriori
+# for X in range(1,10):
+#     print(X)
+
+# for X in "hola como estas":
+#     print(X)
+a = 0
+while(a!=10):
+    print(a) # para cortar un programa que no para se usa: CTRL+C
+    a += 1
+
