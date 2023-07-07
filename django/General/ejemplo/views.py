a0596b5147b6882b324c4a8895fa46f2c90fc757
xlmriosx <xlmriosx@gmail.com>
2023-07-07 17:01:40 -0300

    vista preguntas

--- django/General/ejemplo/views.py
+++ django/General/ejemplo/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+from .models import Pregunta
+
+# def index(request):
+#     pregunta_lista = Pregunta.objects
+#     output = ", ".join([q.pregunta_texto for q in pregunta_lista])
+#     return HttpResponse(output)
+
+def index(request):
+    pregunta_lista = Pregunta.objects.all()
+    template = loader.get_template("ejemplo/index.html")
+    context = {
+        "pregunta_lista": pregunta_lista,
+    }
+    return HttpResponse(template.render(context, request))
